57f7ccbbbb4849b56a7c638c832db16dd2bf9086
multigram <1774728623@qq.com>
2021-07-31 16:47:45 +0800

    update io

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+﻿let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!'
+
+document.querySelector('html').onclick = function() {
+    alert('别戳我，我怕疼。');
+}
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox.png') {
+      myImage.setAttribute('src', 'images/shortcut.png');
+    } else {
+      myImage.setAttribute('src', 'images/firefox.png');
+    }
+}
